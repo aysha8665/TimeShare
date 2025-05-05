59f4f24a1ad38e8f3d371c454d81b8df8b343eb3
aysha8665 <aysha.a@zoho.com>
2025-05-04 20:24:26 -0400

    update config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 /** @type import('hardhat/config').HardhatUserConfig */
+require('dotenv').config();
 require("@nomicfoundation/hardhat-ignition");
 
 module.exports = {
@@ -11,12 +12,12 @@ module.exports = {
     localhost: {
       url: "http://127.0.0.1:8545",
       // Add your wallet's private key here (for deployment)
-      accounts: ["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"],
+      accounts: [process.env.PRIVATE_KEY],
     },
     sepolia: {
-      url: "https://eth-sepolia.g.alchemy.com/v2/_BdhFtEHvIxSC5IUcsJJ2RxQIiV2l8cN", // Replace with your Infura/Alchemy RPC URL
+      url: process.env.ALCHEMY_API_KEY , // Replace with your Infura/Alchemy RPC URL
       chainId: 11155111,
-      accounts: ["f6985be08511e7ab8b1d3a6ddca130b8a7477a1d14a876b04dc76811157a0f00"], // Replace with your private key
+      accounts: [process.env.PRIVATE_KEY2], // Replace with your private key
     },
   }
 };
