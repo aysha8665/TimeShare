7cffd4fe1dfd4c169abb0ed150cdb28898c77cf7
aysha8665 <aysha.a@zoho.com>
2025-05-05 23:59:11 -0400

    change Deploy

--- ignition/modules/Deploy.js
+++ ignition/modules/Deploy.js
@@ -5,36 +5,43 @@ module.exports = buildModule("TimeshareModule", (m) => {
   const deployer = m.getAccount(0);
   console.log("Deploying contracts with account:", deployer);
 
-  // Deploy SmartStayToken
-  const ssToken = m.contract("SmartStayToken", [2025], {
-    from: deployer,
-  });
-  console.log("SmartStayToken deployment initiated");
+  // Deploy SmartStayToken and NFTVault with circular dependency using futures
+  const ssToken = m.contract("SmartStayToken", [2025]);
+  const nftVault = m.contract("NFTVault", [ssToken]);
 
-  // Grant DEFAULT_ADMIN_ROLE with unique ID
-  const adminRole = ethers.ZeroHash;
+  // Grant DEFAULT_ADMIN_ROLE to deployer
+  const adminRole = ethers.ZeroHash; // DEFAULT_ADMIN_ROLE is 0x00...0
   m.call(ssToken, "grantRole", [adminRole, deployer], {
     from: deployer,
     after: [ssToken],
-    id: "GrantAdminRole" // Unique identifier
+    id: "GrantAdminRole"
   });
   console.log("Granted DEFAULT_ADMIN_ROLE to deployer");
 
-  // Deploy ReservationSwap
-  const reservationSwap = m.contract("ReservationSwap", [ssToken], {
+  // Set the NFTVault address in SmartStayToken
+  m.call(ssToken, "SetVaultAddress", [nftVault], {
     from: deployer,
-    after: [ssToken],
+    after: [ssToken, nftVault],
+    id: "SetVaultAddress"
+  });
+  console.log("Set NFTVault address in SmartStayToken");
+
+  // Deploy ReservationSwap with SmartStayToken and NFTVault addresses
+  const reservationSwap = m.contract("ReservationSwap", [ssToken, nftVault], {
+    from: deployer,
+    after: [ssToken, nftVault],
   });
   console.log("ReservationSwap deployment initiated");
 
-  // Grant SWAP_CONTRACT_ROLE with unique ID
+  // Grant SWAP_CONTRACT_ROLE to ReservationSwap
   const swapRole = ethers.keccak256(ethers.toUtf8Bytes("SWAP_CONTRACT_ROLE"));
   m.call(ssToken, "grantRole", [swapRole, reservationSwap], {
     from: deployer,
     after: [reservationSwap],
-    id: "GrantSwapRole" // Unique identifier
+    id: "GrantSwapRole"
   });
-  console.log("Granting SWAP_CONTRACT_ROLE to ReservationSwap");
+  console.log("Granted SWAP_CONTRACT_ROLE to ReservationSwap");
 
-  return { ssToken, reservationSwap };
+  // Return deployed contract instances
+  return { ssToken, nftVault, reservationSwap };
 });
