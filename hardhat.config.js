832ac5cb60650bea32c455ede06a62129cfcb6ed
aysha8665 <aysha.a@zoho.com>
2025-05-07 12:32:43 -0400

    rename param

--- hardhat.config.js
+++ hardhat.config.js
@@ -12,12 +12,12 @@ module.exports = {
     localhost: {
       url: "http://127.0.0.1:8545",
       // Add your wallet's private key here (for deployment)
-      accounts: [process.env.PRIVATE_KEY],
+      accounts: [process.env.PRIVATE_KEY_LOCAL], // Replace with your private key
     },
     sepolia: {
       url: process.env.ALCHEMY_API_KEY , // Replace with your Infura/Alchemy RPC URL
       chainId: 11155111,
-      accounts: [process.env.PRIVATE_KEY2], // Replace with your private key
+      accounts: [process.env.PRIVATE_KEY_SEPOLIA], // Replace with your private key
     },
   }
 };
